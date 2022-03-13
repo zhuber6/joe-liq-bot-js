e882bbaaec354083021adf3962f93cb20525d42f
zhuber6 <zhuber6@gmail.com>
2022-03-12 22:21:59 -0700

    Updated contract address

--- scripts/watchEvents.js
+++ scripts/watchEvents.js
@@ -1,13 +1,9 @@
 const { tokenIdDict, tokenExp, joeToErc20Dict } = require('../common/contractsDict.js');
 
-const joetroller  = "0xdc13687554205E5b89Ac783db14bb5bba4A1eDaC";
-const joeOracle   = "0xd7Ae651985a871C1BC254748c40Ecc733110BC2E";
-const joeRouter   = "0x60aE616a2155Ee3d9A68541Ba4544862310933d4";
-
 const main = async () => {
 
   const FlashLiquidatorFactory = await hre.ethers.getContractFactory('FlashLiquidator');
-  const myLiquidatorContract = await FlashLiquidatorFactory.attach("0xc3e53F4d16Ae77Db1c982e75a937B9f60FE63690");
+  const myLiquidatorContract = await FlashLiquidatorFactory.attach("0x848b3aBd5f73F96baa462a3a794f03D74ED4cf81");
 
   myLiquidatorContract.on("Swap", async (
     tokenIn,
